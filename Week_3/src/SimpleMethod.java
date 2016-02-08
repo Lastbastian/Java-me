6ab11e7e008d39c069944d7e44adcf5384ea7a7e
Chris Bastian <peacethrubeats@gmail.com>
2016-02-08 15:08:50 -0800

    initial commit

---
+++ Week_3/src/SimpleMethod.java
@@ -0,0 +1,24 @@
+//**************************************************
+// SimpleMethod.java
+// inputs       : 
+// outputs      : 
+// Written by   : Chris Bastian
+// Last Modified: 2/8/16
+//**************************************************
+
+public class SimpleMethod {
+	public static void main(String[] args){
+		System.out.println("Hello from the main method.");
+		displayMessage();
+		System.out.println("Back in the main method.");
+	}
+	
+	/**
+	The displayMessage method displays a greeting
+	 */
+	public static void displayMessage()
+	{
+		System.out.println("Hello from the display message method!");
+	}
+	
+}
