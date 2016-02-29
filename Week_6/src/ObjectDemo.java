a425919eead85697d5a0beb0336fa31424b9498d
Chris Bastian <peacethrubeats@gmail.com>
2016-02-29 15:34:34 -0800

    rec object

--- Week_6/src/ObjectDemo.java
+++ Week_6/src/ObjectDemo.java
@@ -9,11 +9,14 @@ public class ObjectDemo
 		
 		int maxNumbers;
 		int number;
-		
+
+		// Create a Scanner object to take user input
 		Scanner keyboard = new Scanner(System.in);
 		
+		// Create a Random object to generate random numbers
 		Random rand = new Random();
 		
+		// Create a PrintWriter object to open a new file
 		PrintWriter outputFile = new PrintWriter("numbers.txt");
 		
 		System.out.println("How many numbers should I write?");
