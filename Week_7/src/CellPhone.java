0ebb47933128dead506c0280ae00edbd5c36a6c6
Chris Bastian <peacethrubeats@gmail.com>
2016-03-06 16:29:43 -0800

    udat

---
+++ Week_7/src/CellPhone.java
@@ -0,0 +1,22 @@
+public class CellPhone
+{
+	private String manufact;
+	private String model;
+	private double retailPrice;
+	
+	public CellPhone(String man, String mod, double price)
+	{
+		manufact = man;
+		model = mod;
+		retailPrice = price;
+		
+	}
+
+
+	setManufact(String man)
+	{
+		manufact = man;
+	}
+	
+	
+}
