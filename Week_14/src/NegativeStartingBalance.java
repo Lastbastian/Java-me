b4024041f394ed6497a2f619b2a465208dd63c7f
Chris Bastian <peacethrubeats@gmail.com>
2016-04-30 21:30:53 -0700

    extends class

---
+++ Week_14/src/NegativeStartingBalance.java
@@ -0,0 +1,12 @@
+
+public class NegativeStartingBalance extends Exception {
+	public NegativeStartingBalance()
+	{
+		super("Error: Negative starting balance");
+	}
+	
+	public NegativeStartingBalance(double amount)
+	{
+		super("Error: Negative starting balance: " + amount);
+	}
+}
