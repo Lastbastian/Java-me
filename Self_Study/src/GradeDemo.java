c13fce8b8be5a113700b05b910c9f7bf8b2fd72c
Chris Bastian <peacethrubeats@gmail.com>
2016-06-18 19:13:29 -0700

    GA update inherit

---
+++ Self_Study/src/GradeDemo.java
@@ -0,0 +1,14 @@
+import javax.swing.JOptionPane;
+
+public class GradeDemo {
+	public static void main(String[] args) {
+		String input;
+		double testScore;
+		
+		GradedActivity grade = new GradedActivity();
+		
+		
+		
+	}
+
+}
